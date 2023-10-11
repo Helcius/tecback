39208b9d76de5acc856ad9db0506f921fcb811bf
Paula Mota <gmotapaula@gmail.com>
2023-10-10 22:45:19 -0300

    Revert "adicionando nova classe"
    This reverts commit 6e8cfcf4009891a8dc4a96da7d4be76e3d3d3fbe.

--- src/main/java/br/com/fujideia/iesp/tecback/repository/CatalogoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fujideia.iesp.tecback.repository;
-
-import br.com.fujideia.iesp.tecback.model.Catalogo;
-import br.com.fujideia.iesp.tecback.model.Avaliacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CatalogoRepository extends JpaRepository<Catalogo,Integer > {
-}
